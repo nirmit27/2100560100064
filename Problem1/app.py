3caae18cb48dbbf77a18016d41f4b75158c88ea2
Nirmit Mishra <mishrayyhsnm2002@gmail.com>
2024-08-22 20:26:51 +0530

    added environment variables

--- Problem1/app.py
+++ Problem1/app.py
@@ -1,30 +1,32 @@
 """ Problem 1 : Average Calculator Microservice """
 
 import os
-import time
 import requests
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
 WINDOW_SIZE = 10
-TEST_SERVER_URL = "http://20.244.56.144/test/"
-TIMEOUT = 0.5
-AUTH_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiZXhwIjoxNzI0MTYyMDk5LCJpYXQiOjE3MjQxNjE3OTksImlzcyI6IkFmZm9yZG1lZCIsImp0aSI6IjYxYWY2Mjk4LTVhYmQtNGMzZi04OTBlLTI0YTVjMTAzOTBhMSIsInN1YiI6Im1pc2hyYW5pcm1pdDI3QGdtYWlsLmNvbSJ9LCJjb21wYW55TmFtZSI6ImZhc3QtYXBwIiwiY2xpZW50SUQiOiI2MWFmNjI5OC01YWJkLTRjM2YtODkwZS0yNGE1YzEwMzkwYTEiLCJjbGllbnRTZWNyZXQiOiJIR2ZxRmlKdVVVc2FhV1JGIiwib3duZXJOYW1lIjoiTmlybWl0IE1pc2hyYSIsIm93bmVyRW1haWwiOiJtaXNocmFuaXJtaXQyN0BnbWFpbC5jb20iLCJyb2xsTm8iOiIyMTAwNTYwMTAwMDY0In0.6f0C8xyveYk4ztXX8MWbEAUn_LZThi7kX6DVD8aLT5g"
+TIMEOUT: float = 0.5
+AUTH_TOKEN: str | None = os.environ.get("AUTH_TOKEN")
+TEST_SERVER_URL: str | None = os.environ.get("TEST_SERVER_URL")
 
 window: list = []
 
+
 def fetch_numbers(number_id):
-    headers = {
-        "Authorization": f"Bearer {AUTH_TOKEN}"
-    }
     try:
-        response = requests.get(f"{TEST_SERVER_URL}/{number_id}", headers=headers, timeout=TIMEOUT)
+        headers = {
+            "Authorization": f"Bearer {AUTH_TOKEN}"
+        }
+        response = requests.get(
+            f"{TEST_SERVER_URL}/{number_id}", headers=headers, timeout=TIMEOUT)
         if response.status_code == 200:
             return response.json().get("numbers", [])
-    except requests.exceptions.RequestException:
-        pass
+    except requests.exceptions.RequestException as e:
+        print(e)
     return []
+
 
 @app.route('/numbers/<number_id>', methods=['GET'])
 def get_numbers(number_id):
@@ -54,6 +56,7 @@ def get_numbers(number_id):
     }
 
     return jsonify(response)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
