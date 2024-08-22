27b1169f65ed14687d0b8cf54d80e1276dc780fc
Nirmit Mishra <mishrayyhsnm2002@gmail.com>
2024-08-22 20:20:24 +0530

    Over and out

--- Problem1/app.py
+++ Problem1/app.py
@@ -1,14 +1,15 @@
 """ Problem 1 : Average Calculator Microservice """
 
-from flask import Flask, jsonify, request
-import requests
+import os
 import time
+import requests
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
 WINDOW_SIZE = 10
 TEST_SERVER_URL = "http://20.244.56.144/test/"
-TIMEOUT = 0.5  # 500 ms
+TIMEOUT = 0.5
 AUTH_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiZXhwIjoxNzI0MTYyMDk5LCJpYXQiOjE3MjQxNjE3OTksImlzcyI6IkFmZm9yZG1lZCIsImp0aSI6IjYxYWY2Mjk4LTVhYmQtNGMzZi04OTBlLTI0YTVjMTAzOTBhMSIsInN1YiI6Im1pc2hyYW5pcm1pdDI3QGdtYWlsLmNvbSJ9LCJjb21wYW55TmFtZSI6ImZhc3QtYXBwIiwiY2xpZW50SUQiOiI2MWFmNjI5OC01YWJkLTRjM2YtODkwZS0yNGE1YzEwMzkwYTEiLCJjbGllbnRTZWNyZXQiOiJIR2ZxRmlKdVVVc2FhV1JGIiwib3duZXJOYW1lIjoiTmlybWl0IE1pc2hyYSIsIm93bmVyRW1haWwiOiJtaXNocmFuaXJtaXQyN0BnbWFpbC5jb20iLCJyb2xsTm8iOiIyMTAwNTYwMTAwMDY0In0.6f0C8xyveYk4ztXX8MWbEAUn_LZThi7kX6DVD8aLT5g"
 
 window: list = []
